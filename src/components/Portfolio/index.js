381441373f83131d2c242e502f7d148b0f57949e
ChiaraUsa <chiaravivianvalo@gmail.com>
2024-07-27 11:44:32 -0500

    Cambio de ruta de proyectos

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
-import './index.scss'
+import './index.scss';
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import portfolioData from '../../data/portfolio.json'
+import portfolioData from '../../data/portfolio.json';
+
+// Importar imágenes
+import EEG from '../../assets/projects/EEG.png';
+import Reconocimiento from '../../assets/projects/reconocimiento.jpeg';
+import RUTA from '../../assets/projects/RUTA.png';
+
+const images = {
+    "EEG.png": EEG,
+    "reconocimiento.jpeg": Reconocimiento,
+    "RUTA.png": RUTA,
+};
 
 const Portfolio = () => {
-
-    const [letterClass, setLetterClass] = useState('text-animate')
+    const [letterClass, setLetterClass] = useState('text-animate');
 
     useEffect(() => {
         const timeout = setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
+            setLetterClass('text-animate-hover');
+        }, 3000);
         return () => clearTimeout(timeout);  // Cleanup the timeout
-    }, [])
+    }, []);
 
     const renderPortfolio = (portfolio) => {
         return (
@@ -23,7 +33,7 @@ const Portfolio = () => {
                         return (
                             <div className="box" key={idx}>
                                 <div className="face front">
-                                    <img src={port.cover} className="portfolio-image" alt="portfolio"/>
+                                    <img src={images[port.cover]} className="portfolio-image" alt="portfolio" />
                                     <h3>{port.title}</h3>
                                 </div>    
                                 <div className="face back">
@@ -32,24 +42,24 @@ const Portfolio = () => {
                                     <button className="btn" onClick={() => window.open(port.url)}>Abrir código fuente</button>
                                 </div>
                             </div>
-                        )
+                        );
                     })
                 }
             </div>
         );
-    }
+    };
 
     return (
         <>
-        <div className="container portfolio-page">
-            <h1 className="page-title">
-                <AnimatedLetters strArray={"Proyectos".split("")} idx={15} letterClass={letterClass} />
-            </h1>
-            <div>{renderPortfolio(portfolioData.portfolio)}</div>
-        </div>
-        <Loader type="pacman" />
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters strArray={"Proyectos".split("")} idx={15} letterClass={letterClass} />
+                </h1>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            </div>
+            <Loader type="pacman" />
         </>
-    )   
-}
+    );
+};
 
-export default Portfolio
+export default Portfolio;
