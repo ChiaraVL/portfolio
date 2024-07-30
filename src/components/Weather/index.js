d4be6faaa81ebb27641e0dddc22fc5f549538279
ChiaraUsa <chiaravivianvalo@gmail.com>
2024-07-30 12:15:13 -0500

    Implementación de muestra del clima con ciudad

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -3,12 +3,73 @@ import { faLocationDot, faSearch, faWater, faWind } from '@fortawesome/free-soli
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from "react-loaders";
-import Cloud from '../../assets/images/cloud.png';
+import Clear from '../../assets/images/clear.png';
+import Rain from '../../assets/images/rain.png';
+import Snow from '../../assets/images/snow.png';
+import Clouds from '../../assets/images/cloud.png';
+import Mist from '../../assets/images/mist.png';
+import NF from '../../assets/images/404.png'
 import './index.scss';
 
 const Weather = () => {
 
+    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+    const [notFound, setNotFound] = useState(false);
     const [letterClass, setLetterClass] = useState('text-animate');
+    const APIKey = '971587d23518daba5cda5745cf0e83ed';
+
+    const fetchWeatherData = () => {
+        if (city === '') return;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&lang=es`)
+            .then(response => response.json())
+            .then(json => {
+
+                if (json.cod === '404') {
+                    setNotFound(true);
+                    setWeatherData(null);
+                    return;
+                }
+
+                setNotFound(false);
+
+                let weatherImage;
+                switch (json.weather[0].main) {
+                    case 'Clear':
+                        weatherImage = Clear;
+                        break;
+                    case 'Rain':
+                        weatherImage = Rain;
+                        break;
+                    case 'Snow':
+                        weatherImage = Snow;
+                        break;
+                    case 'Clouds':
+                        weatherImage = Clouds;
+                        break;
+                    case 'Mist':
+                        weatherImage = Mist;
+                        break;
+                    default:
+                        weatherImage = Clouds;
+                        break;
+                }
+
+                setWeatherData({
+                    image: weatherImage,
+                    temperature: json.main.temp,
+                    description: json.weather[0].description,
+                    humidity: json.main.humidity,
+                    windSpeed: json.wind.speed,
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+                setNotFound(true); // Handle fetch errors as not found
+                setWeatherData(null);
+            });
+    };
 
     useEffect(() => {
         const timeout = setTimeout(() => {
@@ -21,46 +82,62 @@ const Weather = () => {
         <>
             <div className='container weather-page'>
                 <h1 className="page-title">
-                        <AnimatedLetters strArray={"Clima".split("")} idx={15} letterClass={letterClass} />
+                    <AnimatedLetters strArray={"Clima".split("")} idx={15} letterClass={letterClass} />
                 </h1>
                 <div className='scontainer'>
                     <div className='search-box'>
-                        <FontAwesomeIcon className='icon' icon={faLocationDot} color='#211F1C'/>
-                        <input type='text' placeholder='Ingresa tu ubicación' />
-                        <button className='search'>
-                            <FontAwesomeIcon icon={faSearch} color='#211F1C'/>
+                        <FontAwesomeIcon className='icon' icon={faLocationDot} color='#E3A5C7'/>
+                        <input 
+                            type='text' 
+                            placeholder='Ingresa tu ubicación' 
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                        <button onClick={fetchWeatherData}>
+                            <FontAwesomeIcon icon={faSearch} color='#E3A5C7'/>
                         </button>
                     </div>
-                    <div className='weather-box'>
-                        <div className='info-weather'>
-                            <div className='weather'>
-                                <img src={Cloud} alt='clima'/>
-                                <p className='temperature'>16<span>°C</span></p>
-                                <p className='description'>Nubes Rotas</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='weather-details'>
-                        <div className='humidity'>
-                            <FontAwesomeIcon className='icono' icon={faWater}/>
-                            <div className='text'>
-                                <div className='info-humidity'>
-                                    <span>0%</span>
+                    {weatherData && (
+                        <div className='weather-box'>
+                            <div className='info-weather'>
+                                <div className='weather'>
+                                    <img src={weatherData.image} alt='clima'/>
+                                    <p className='temperature'>{weatherData.temperature}<span>°C</span></p>
+                                    <p className='description'>{weatherData.description}</p>
                                 </div>
-                                <p>Humedad</p>
                             </div>
                         </div>
-                        <div className='wind'>
-                            <FontAwesomeIcon className='icono' icon={faWind}/>
-                            <div className='text'>
-                                <div className='info-wind'>
-                                    <span>0Km/h</span>
+                    )}
+                    {weatherData && (
+                        <div className='weather-details'>
+                            <div className='humidity'>
+                                <FontAwesomeIcon className='icono' icon={faWater}/>
+                                <div className='text'>
+                                    <div className='info-humidity'>
+                                        <span>{weatherData.humidity}%</span>
+                                    </div>
+                                    <p>Humedad</p>
                                 </div>
-                                <p>Velocidad Viento</p>
+                            </div>
+                            <div className='wind'>
+                                <FontAwesomeIcon className='icono' icon={faWind}/>
+                                <div className='text'>
+                                    <div className='info-wind'>
+                                        <span>{weatherData.windSpeed}Km/h</span>
+                                    </div>
+                                    <p>Vientos</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
+                    {notFound && (
+                        <div className='not-found'>
+                            <div className='box'>
+                                <img src={NF} alt='No se encuentra'/>
+                                <p>Upps! <br/> No encontramos la ubicación!</p>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
             <Loader type='pacman'/>
@@ -68,4 +145,4 @@ const Weather = () => {
     )
 }
 
-export default Weather
+export default Weather;
