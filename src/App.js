6c3573108b662c2267f8d8ae5f4436c40fb7b493
Chiara V. Valenzuela L. <chiaravivianvalo@gmail.com>
2025-01-07 15:03:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route index element={<Home/>} />
         <Route path='about' element={<About/>} />
         <Route path='contact' element={<Contact/>} />
-        <Route path='portfolio' element={<Portfolio/>} />
+        <Route path='projects' element={<Portfolio/>} />
         <Route path='weather' element={<Weather/>} />
       </Route>
     </Routes>
